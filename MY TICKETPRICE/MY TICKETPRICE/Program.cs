d7d980579e867f19d9143c79862cbeaede63b80c
jonas-anaisie <137724022+jonas-anaisie@users.noreply.github.com>
2024-06-28 16:07:52 +0000

    Add files via upload

---
+++ MY TICKETPRICE/MY TICKETPRICE/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class TicketPriceCalculator
+{
+static void Main()
+{
+    const int regularPrice = 10;
+    const int discountedPrice = 7;
+
+    Console.Write("Enter your age: ");
+    int age = int.Parse(Console.ReadLine());
+
+    int ticketPrice = (age <= 12 || age >= 65) ? discountedPrice : regularPrice;
+    Console.Write($"The ticket price is GHC{ticketPrice}");
+
+}
+
+
+
+
+}
